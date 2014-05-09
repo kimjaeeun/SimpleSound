1e7a753c43e1439f3c24e23b09ee206f5a512d50
kimjaeeun <geeins55@naver.com>
2014-05-09 10:39:28 +0900

    add fix

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-	$("#bird").click(function() {
-		$(this).toggleClass("state1");
-		$(this).toggleClass("state2");
-
-		$("#birdSound")[0].load();
-		$("#birdSound")[0].play();
+	$(".btn").click(function() {
+		
+		$("#btnSound")[0].load();
+		$("#btnSound")[0].play();
 	});
 });
